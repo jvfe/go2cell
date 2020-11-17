6323676e022148cf97312e31683fc1d4bcd6bc81
jvfe <jvfecav@gmail.com>
2020-11-17 16:12:07 -0300

    style: Minor style changes with formatR

--- R/go2cell.R
+++ R/go2cell.R
@@ -17,37 +17,37 @@
 #'    columns correspond to the Gene Ontology IDs given, and the last column
 #'    corresponds to the cell type's marker that led to the result.
 #'
-#' @seealso \url{https://www.wikidata.org/} for information regarding Wikidata and
-#'   \url{https://panglaodb.se/index.html} for the original database this information
-#'   was adapted from.
+#' @seealso \url{https://www.wikidata.org/} for information regarding
+#'   Wikidata and \url{https://panglaodb.se/index.html} for the
+#'   original database this information was adapted from.
 #'
 #' @export
 #'
 #' @examples
-#' # Return cell types related to "muscle contraction" and "metabolism"
-#' go2cell(c("GO:0006936", "GO:0008152"))
+#' # Return cell types related to 'muscle contraction' and 'metabolism'
+#' go2cell(c('GO:0006936', 'GO:0008152'))
 #'
-#' # Return cell types related to "response to zinc ion"
-#' go2cell("GO:0010043")
+#' # Return cell types related to 'response to zinc ion'
+#' go2cell('GO:0010043')
 #' \dontrun{
 #' # IDs should use ':' not '_'
-#' go2cell("GO_0010043")
+#' go2cell('GO_0010043')
 #' }
 go2cell <- function(go_ids) {
-  go_ids_collapsed <- .collapse_as_values(go_ids, quotes = TRUE)
-
-  genes_from_go <- .get_genes_from_go(go_ids_collapsed)
-
-  gene_values <- .collapse_as_values(genes_from_go$gene)
-
-  celltypes <- .query_ctp_turtle(gene_values)
-
-  celltypes_collapsed <- .collapse_as_values(celltypes$cell_type)
-
-  celltype_wdt <- .get_celltype_items(celltypes_collapsed)
-
-  final_table <- celltypes %>%
-    dplyr::inner_join(genes_from_go, by = "gene") %>%
-    dplyr::left_join(celltype_wdt, by = c("cell_type")) %>%
-    dplyr::select(cell_type, cell_typeLabel, go_ids, go_termLabel, geneLabel)
+    go_ids_collapsed <- .collapse_as_values(go_ids, quotes = TRUE)
+    
+    genes_from_go <- .get_genes_from_go(go_ids_collapsed)
+    
+    gene_values <- .collapse_as_values(genes_from_go$gene)
+    
+    celltypes <- .query_ctp_turtle(gene_values)
+    
+    celltypes_collapsed <- .collapse_as_values(celltypes$cell_type)
+    
+    celltype_wdt <- .get_celltype_items(celltypes_collapsed)
+    
+    final_table <- celltypes %>% dplyr::inner_join(genes_from_go, 
+        by = "gene") %>% dplyr::left_join(celltype_wdt, by = c("cell_type")) %>% 
+        dplyr::select(cell_type, cell_typeLabel, go_ids, go_termLabel, 
+            geneLabel)
 }
--- R/queries.R
+++ R/queries.R
@@ -4,17 +4,14 @@
 #'
 #' @return A dataframe of GO ids and genes
 #' @examples
-#' .get_genes_from_go("('GO:0006936')")
+#' .get_genes_from_go('('GO:0006936')')
 .get_genes_from_go <- function(go_ids) {
-  if (any(grepl("GO:\\d+", go_ids) == FALSE)) {
-    stop(
-      "A GO identifier given doesn't match the regex 'GO:\\d+', check your input for inconsistencies."
-    )
-  }
-
-  message("Querying wikidata for GO/gene correspondence...")
-  go_query <- sprintf(
-    'SELECT ?gene ?go_ids ?go_termLabel ?geneLabel
+    if (any(grepl("GO:\\d+", go_ids) == FALSE)) {
+        stop("A GO identifier given doesn't match the regex 'GO:\\d+', check your input for inconsistencies.")
+    }
+    
+    message("Querying wikidata for GO/gene correspondence...")
+    go_query <- sprintf("SELECT ?gene ?go_ids ?go_termLabel ?geneLabel
       WHERE
       {
         VALUES (?go_ids) {%s}
@@ -22,15 +19,12 @@
         ?protein wdt:P703 wd:Q15978631;
                  ?godomain ?go_term;
                  wdt:P702 ?gene.
-        SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
-      }',
-    go_ids
-  )
-
-  WikidataQueryServiceR::query_wikidata(go_query) %>%
-    dplyr::mutate(
-      gene = .remove_wdt_url(gene),
-    )
+        SERVICE wikibase:label { bd:serviceParam wikibase:language \"en\". }
+      }", 
+        go_ids)
+    
+    WikidataQueryServiceR::query_wikidata(go_query) %>% dplyr::mutate(gene = .remove_wdt_url(gene), 
+        )
 }
 
 #' Query local database for cell types related to particular markers
@@ -39,30 +33,23 @@
 #'
 #' @return A dataframe of cell type Wikidata items and their respective markers
 #' @examples
-#' .query_ctp_turtle("(wd:Q14881255) (wd:Q18016342)")
+#' .query_ctp_turtle('(wd:Q14881255) (wd:Q18016342)')
 .query_ctp_turtle <- function(gene_items) {
-  celltype_marker_graph <- rdflib::rdf_parse(celltype_marker_turtle,
-    format = c("turtle")
-  )
-
-  ctp_sparql <-
-    sprintf(
-      "PREFIX ctp: <http://celltypes.wiki.opencura.com/entity/>
+    celltype_marker_graph <- rdflib::rdf_parse(celltype_marker_turtle, 
+        format = c("turtle"))
+    
+    ctp_sparql <- sprintf("PREFIX ctp: <http://celltypes.wiki.opencura.com/entity/>
                      PREFIX wd: <http://www.wikidata.org/entity/>
                        SELECT ?cell_type ?gene
                        WHERE {
                         VALUES (?genes) {%s}
                         BIND (?genes AS ?gene)
                         ?cell_type ctp:P9 ?gene .
-                       }",
-      gene_items
-    )
-
-  rdflib::rdf_query(celltype_marker_graph, ctp_sparql) %>%
-    dplyr::mutate(
-      cell_type = .remove_wdt_url(cell_type),
-      gene = .remove_wdt_url(gene)
-    )
+                       }", 
+        gene_items)
+    
+    rdflib::rdf_query(celltype_marker_graph, ctp_sparql) %>% dplyr::mutate(cell_type = .remove_wdt_url(cell_type), 
+        gene = .remove_wdt_url(gene))
 }
 
 #' Query Wikidata for cell type item names from cell type QIDs
@@ -71,20 +58,17 @@
 #'
 #' @return A dataframe of QIDs and their respective labels
 #' @examples
-#' .get_celltype_items("(wd:Q101405098) (wd:Q101404862)")
+#' .get_celltype_items('(wd:Q101405098) (wd:Q101404862)')
 .get_celltype_items <- function(celltype_ids) {
-  message("Querying wikidata for cell type labels...")
-  celltype_sparql <- sprintf(
-    'SELECT ?cell_type ?cell_typeLabel
+    message("Querying wikidata for cell type labels...")
+    celltype_sparql <- sprintf("SELECT ?cell_type ?cell_typeLabel
   WHERE
   {
     VALUES (?cell_type) {%s}
-    SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
-  }',
-    celltype_ids
-  )
-
-  celltype_wdt <-
-    WikidataQueryServiceR::query_wikidata(celltype_sparql) %>%
-    dplyr::mutate(cell_type = .remove_wdt_url(cell_type))
+    SERVICE wikibase:label { bd:serviceParam wikibase:language \"en\". }
+  }", 
+        celltype_ids)
+    
+    celltype_wdt <- WikidataQueryServiceR::query_wikidata(celltype_sparql) %>% 
+        dplyr::mutate(cell_type = .remove_wdt_url(cell_type))
 }
