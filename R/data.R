7f39fb6f149f0a6928c64d85844ce20279f9d1da
jvfe <jvfecav@gmail.com>
2020-11-20 09:09:29 -0300

    feat: Add small testing dataset

--- R/data.R
+++ R/data.R
@@ -10,3 +10,15 @@
 #' @author
 #' Tiago Lubiana
 "celltype_marker_turtle"
+
+#' A dataframe of Gene symbols and GO IDs
+#'
+#' Small dataset for testing and showcase purposes.
+#'
+#' @format A dataframe of 3 columns, corresponding to
+#'    a gene name, a go ID process term and the go ID
+#'    itself.
+#'
+#' @author
+#' João Vitor F Cavalcante
+"go2cell_test_data"
--- data-raw/preprocess.R
+++ data-raw/preprocess.R
@@ -3,4 +3,7 @@ ttl_file <- "data-raw/celltype_marker_graph.ttl"
 
 celltype_marker_turtle <- readChar(ttl_file, file.info(ttl_file)$size)
 
+go2cell_test_data <- read.csv("data-raw/go_small_dataset.csv")
+
 usethis::use_data(celltype_marker_turtle, overwrite = TRUE)
+usethis::use_data(go2cell_test_data, overwrite = TRUE)
