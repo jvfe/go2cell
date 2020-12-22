177334cbea64c7b11e64b7de3411abf8c1f6228b
jvfe <jvfecav@gmail.com>
2020-12-22 11:11:34 -0300

    refactor: Remove unnecessary source code & data
    Removing auxiliary queries and local rdf database, as well as rdflib
    dependency, that won't be necessary.

--- R/data.R
+++ R/data.R
@@ -1,16 +1,3 @@
-#' RDF graph of cell type - marker correspondence
-#'
-#' A dataset consisting of a Turtle-syntax RDF graph
-#' of human cell types and their corresponding markers,
-#' extracted, manually curated and serialized from
-#' \url{https://panglaodb.se/} database
-#'
-#' @format A character vector of a Turtle RDF graph
-#' @source \url{https://panglaodb.se/}
-#' @author
-#' Tiago Lubiana
-"celltype_marker_turtle"
-
 #' A dataframe of Gene symbols and GO IDs
 #'
 #' Small dataset for testing and showcase purposes.
